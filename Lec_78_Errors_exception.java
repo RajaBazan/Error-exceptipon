c9d4f8fde7261a6e7f3a13c9c4d29f6962e5a0a1
Muhammad Bazan <112274805+RajaBazan@users.noreply.github.com>
2023-08-17 01:57:13 +0500

    Add files via upload

---
+++ Lec_78_Errors_exception.java
@@ -0,0 +1,11 @@
+package hello.com;
+
+public class Lec_78_Errors_exception {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+                int c;
+        c= a +b;
+        System.out.println(c);
+    }
+}
